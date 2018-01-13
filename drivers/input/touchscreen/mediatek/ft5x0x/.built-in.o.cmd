7b23f3ffe47e9fa245184c21001b805c784a1c9d
Skyrimus <skyrimus@yandex.ru>
2018-01-13 12:48:25 +0300

    Fix speaker and headphones switch

--- drivers/input/touchscreen/mediatek/ft5x0x/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_drivers/input/touchscreen/mediatek/ft5x0x/built-in.o :=  /root/linaro/bin/arm-cortex_a7-linux-gnueabihf-ld -EL    -r -o drivers/input/touchscreen/mediatek/ft5x0x/built-in.o drivers/input/touchscreen/mediatek/ft5x0x/ft5x0x_driver.o 
